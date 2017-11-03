73db6f61d2c1254570c9c2c75750ebd3a43fb85e
Foxscotch <foxscotch@gmail.com>
2017-11-03 17:06:49 -0500

    Progress on locales.js

--- locales.js
+++ locales.js
@@ -1,6 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
 const _ = require('lodash');
 
-const locales = require('./locales/meta.json');
+
+const localesFolder = './locales';
 
 
-function compile() {}
+class Locales {
+  constructor(locales, default) {
+    this.locales = [];
+    this.default = default;
+
+    for (let locale of locales) {
+      this.locales.push(Locale(locale.code, locale.name));
+    }
+  }
+
+  getText(textName, context={}, locale=this.default) {
+    return this.text[locale][textName](context)
+  }
+
+  compileTemplate(path) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
+        if (err)
+          reject(err);
+        else
+          resolve(_.template(data));
+      });
+    });
+  }
+}
+
+
+class Locale {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+}
