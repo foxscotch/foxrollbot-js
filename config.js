d0f50da69b0dab0da3a9c393ec221c0afaf6d1be
Foxscotch <foxscotch@gmail.com>
2017-11-07 13:51:56 -0600

    Remove some environment variables from config.js
    Given that I have values that won't accept env vars, I don't think it
    makes sense for other small stuff to use them either. As of right now, the
    only env var it uses is for the location of the config file itself.

--- config.js
+++ config.js
@@ -1,14 +1,16 @@
-const configFile = require('js-yaml').load(
+const process = require('process');
+const yaml = require('js-yaml');
+
+
+const configFile = yaml.load(
   fs.readFileSync(process.env['FRB_CONFIG_FILE'] || './config.yaml', 'utf8')
 );
 
 module.exports = {
   locale: {
-    locales:    configFile.locale.locales, // No default! You MUST use a config file!
-                                           // If this value isn't in this config file, locales.js
-                                           // will check in localesDir/meta.json.
-    default:    process.env['FRB_DEFAULT_LOCALE'] || configFile.locale.default    || 'en-US',
-    localesDir: process.env['FRB_LOCALES_DIR']    || configFile.locale.localesDir || './locales',
+    locales:          configFile.locale.locales, // No default! You MUST use a config file.
+    default:          configFile.locale.default          || 'en-US',
+    localesDir:       configFile.locale.localesDir       || './locales',
     interpolateRegEx: configFile.locale.interpolateRegEx || /{{([\s\S]+?)}}/g,
     evaluateRegEx:    configFile.locale.evaluateRegEx    || /{%([\s\S]+?)%}/g
   }
