51919c21a927e51a48c7fd235a946d542501fef3
Foxscotch <foxscotch@gmail.com>
2017-11-08 17:01:10 -0600

    Add logging.js

---
+++ logging.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+
+const _ = require('lodash');
+
+const conf = require('./config');
+
+
+/**
+ * Interface for writing logs.
+ * @interface
+ */
+class ILogWriter {
+  /**
+   * Logs the log entry.
+   * @param {number|Date} timestamp - Time of log. If a Date is passed, it should use timestamp.getTime().
+   * @param {string} text - Text to log.
+   */
+  log(timestamp, text) {
+    throw new Error('Not implemented.');
+  }
+}
+
+
+/**
+ * Class for writing logs to files.
+ * @implements ILogWriter
+ * @property {string} level - Log level for this writer to write at.
+ * @property {string} file - Path of file to write logs to.
+ * @property {string} template - Template that logs are written with.
+ */
+class FileLogWriter {
+  /**
+   * Constructs a FileLogWriter.
+   * @param {string} level - Log level for this writer to write at.
+   * @param {string} path - Path of file to write logs to.
+   * @param {string} [format='{{ level }} {{ timestamp }}: {{ text }}'] - String for format of logs. A newline is added automatically, so you shouldn't add one yourself.
+   */
+  constructor(level, path, format='{{ level }} {{ timestamp }}: {{ text }}') {
+    this.level = level;
+    this.file = fs.createWriteStream(path, { mode: 'a', encoding: 'utf8' });
+    this.template = _.template(format + '\n', {
+      interpolate: conf.locale.interpolateRegEx
+    });
+  }
+}
+
+
+class Logger {
+  constructor() {}
+}
